5173a86467fc5b74e45a6e5e6421ab0df5c8e373
Anjali Ravindran <anjali93ravindran@gmail.com>
2022-02-19 00:33:31 +0530

    change

--- casestudy.js
+++ casestudy.js
@@ -5,23 +5,29 @@ xhttp.onreadystatechange= function(){
     if (this.readyState==4 && this.status==200){
         var response=JSON.parse(this.responseText);
         var output=" ";
+        var count=0;
+        var count1=0;
         for(var i=0;i<response.length;i++){
-            console.log(i);
+            // console.log(i);
             var res=response[i].title;
             var stat=response[i].completed;
-            console.log(stat);
+            // console.log(stat);
+            
             if (stat ==true){
-                console.log('this is executing')
-                output +="<li><input type='checkbox' checked>" + res+"</li>";
+                // console.log('this is executing')
+                output +="<li><input type='checkbox' id='check' checked >" + res+"</li>";
+                count=count+1;
             }
             else if(stat==false){
-                console.log('this is also executing')
-                output +="<li><input type='checkbox'>" + res+"</li>";
+                output +="<li><input type='checkbox' id='check' onchange='doalert(check)'>" + res+"</li>";
             }
-            // console.log('that');
+
+            console.log(count);
                
 
         }
+        
+        
         document.getElementById('demo').innerHTML=output;
     }
 }
@@ -31,4 +37,16 @@ xhttp.send();
 function loggedOut(){
     alert("Are you sure to log-out?")
 }
-
+function doalert(checkboxElem) {
+    // console.log(count1);
+  if (checkboxElem.checked) {
+    alert ("hi");
+    
+  } else {
+    
+    alert(`One Todo is completed`);
+    count1=count1+1;
+    return count1;
+    
+  }
+}
